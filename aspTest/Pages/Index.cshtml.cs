4120e6f7378df2a40efcd12ff3811e98eed6241d
JungBok Cho <chojb2015@gmail.com>
2020-05-12 18:59:10 -0700

    Initial Creation

---
+++ aspTest/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using aspTest.Models;
+using aspTest.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace aspTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
---
+++ aspTest/Pages/Listings.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspTest.Pages
+{
+    public class ListingsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
