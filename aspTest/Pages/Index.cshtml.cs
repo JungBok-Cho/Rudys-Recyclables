62fe9d2eab5dd25b62edac5b673093b57faccaea
JungBok Cho <chojb2015@gmail.com>
2020-05-24 17:58:50 -0700

    Add comments to the Index Page

--- aspTest/Pages/Index.cshtml.cs
+++ aspTest/Pages/Index.cshtml.cs
@@ -10,18 +10,38 @@ using Microsoft.Extensions.Logging;
 
 namespace aspTest.Pages
 {
+    /// <summary>
+    /// The index Page
+    /// </summary>
     public class IndexModel : PageModel
     {
+        // local Logger reference
         private readonly ILogger<IndexModel> _logger;
+
+        // The product service reference
         public JsonFileProductService ProductService;
+
+        // The list of products
         public IEnumerable<Product> Products { get; private set; }
 
+        /// <summary>
+        /// Constructor
+        /// Injecting the Logger
+        /// Injecting the Product Service service
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="productService"></param>
         public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
         {
             _logger = logger;
             ProductService = productService;
         }
 
+        /// <summary>
+        /// Get for the Index page
+        /// 
+        /// Returns all the products for reviewing
+        /// </summary>
         public void OnGet()
         {
             Products = ProductService.GetProducts();
