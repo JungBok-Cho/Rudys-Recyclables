e1a5a93ee85da6ab19c6c683a3219247b623041e
JungBok Cho <chojb2015@gmail.com>
2020-05-24 18:00:25 -0700

    Add comments to the Item page

--- aspTest/Pages/Item.cshtml.cs
+++ aspTest/Pages/Item.cshtml.cs
@@ -7,8 +7,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RudysRecyclables.Pages
 {
+    /// <summary>
+    /// The Item Page
+    /// </summary>
     public class ItemModel : PageModel
     {
+        /// <summary>
+        /// Get for the Item Page
+        /// </summary>
         public void OnGet()
         {
 
