42fb2f5c5d0c32c0a28bdfd9db65dc39ef431540
JungBok Cho <chojb2015@gmail.com>
2020-05-24 18:08:36 -0700

    Add comments to the Profile page

--- aspTest/Pages/Profile.cshtml.cs
+++ aspTest/Pages/Profile.cshtml.cs
@@ -7,8 +7,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RudysRecyclables.Pages
 {
+    /// <summary>
+    /// The Profile Page
+    /// </summary>
     public class ProfileModel : PageModel
     {
+        /// <summary>
+        /// Get for the Profile Page
+        /// </summary>
         public void OnGet()
         {
 
