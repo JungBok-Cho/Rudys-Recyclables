c2ef72479b8b49d234c9466dee2c24d82ae01e82
Ruifeng Wang <rwang@seattleu.edu>
2020-05-24 23:08:50 -0700

    Add AdminHome.js file

---
+++ aspTest/wwwroot/js/AdminHome.js
@@ -0,0 +1,31 @@
+﻿var today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+$('#startDate').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    dateFormat: 'mm-dd-yyyy',
+    minDate: "@DateTime.Today.AddDays(-14).ToShortDateString()",
+    maxDate: today,
+});
+$('#endDate').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    minDate: function () {
+        return $('#startDate').val();
+    },
+    maxDate: today
+});
+$('#startDateTwo').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    dateFormat: 'mm-dd-yyyy',
+    minDate: "@DateTime.Today.AddDays(-14).ToShortDateString()",
+    maxDate: today,
+});
+$('#endDateTwo').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    minDate: function () {
+        return $('#startDate').val();
+    },
+    maxDate: today
+});
