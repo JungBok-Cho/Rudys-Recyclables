d3d440311fdaf148f049c6a78a90c0a4e714563d
JungBok Cho <chojb2015@gmail.com>
2020-05-24 18:16:00 -0700

    Add comments to the Error page

--- aspTest/Pages/Error.cshtml.cs
+++ aspTest/Pages/Error.cshtml.cs
@@ -9,20 +9,35 @@ using Microsoft.Extensions.Logging;
 
 namespace aspTest.Pages
 {
+    /// <summary>
+    /// The Error Page
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
+        // getter and setter for the request id
         public string RequestId { get; set; }
 
+        // The show request id reference
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+        // local Logger reference
         private readonly ILogger<ErrorModel> _logger;
 
+        /// <summary>
+        /// Constructor
+        /// Injecting the Logger
+        /// </summary>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Get for the Error page
+        /// 
+        /// Returns the request id related to an error
+        /// </summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
