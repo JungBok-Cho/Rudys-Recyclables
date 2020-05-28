37c48f4f3d339ae65af01e515295f292d75fc288
Ruifeng Wang <rwang@seattleu.edu>
2020-05-28 14:20:05 -0700

    Add js code from AdminHome.js to site.js
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/boaJic0cX0Ss567G9JgbvGQAN1lt?Type=TaskLink&Channel=Link&CreatedTime=637262975809180000

--- aspTest/wwwroot/js/site.js
+++ aspTest/wwwroot/js/site.js
@@ -2,6 +2,57 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+var imageOne = document.getElementById("graphOne");
+
+function ChangeGraphOne()
+{
+    if (imageOne.getAttribute('src') == "/img/PrimaryGraphOne.png")
+    {
+        imageOne.src = "/img/SecondaryGraphOne.png";
+    }
+    else
+    {
+        imageOne.src = "/img/PrimaryGraphOne.png";
+    }
+}
+
+var today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+
+$('#startDate').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    dateFormat: 'mm-dd-yyyy',
+    minDate: "05-01-2020",
+    maxDate: today,
+});
+
+$('#endDate').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    minDate: function () {
+        return $('#startDate').val();
+    },
+    maxDate: today
+});
+
+$('#startDateTwo').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    dateFormat: 'mm-dd-yyyy',
+    minDate: "05-01-2020",
+    maxDate: today,
+});
+
+$('#endDateTwo').datepicker({
+    uiLibrary: 'bootstrap4',
+    iconsLibrary: 'fontawesome',
+    minDate: function () {
+        return $('#startDate').val();
+    },
+    maxDate: today
+});
+
 function searchBar() {
     let val = searchtxt.value;
     if (val === "lamp") {
@@ -11,11 +62,11 @@ function searchBar() {
     } else {
         window.location.href = 'ItemNotFound';
     }
-}
+};
 
 function contacted() {
     var button = document.getElementById("ContactButton");
     window.alert("Poster has been contacted!");
     button.disabled = true;
     button.value = "Pending";
-}
+};
