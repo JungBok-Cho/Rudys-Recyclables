7495e0afbbe960883f8e727e4b3f8be9abb32159
Grey Kumar <gkumar@seattleu.edu>
2020-05-27 20:41:20 -0700

    Added script from index to site.js
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/DqF4msaOLU2hnjtEPqq6IGQAL5VY?Type=TaskLink&Channel=Link&CreatedTime=637262340535170000

--- aspTest/wwwroot/js/site.js
+++ aspTest/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+function myFunc() {
+    let val = searchtxt.value;
+    if (val === "lamp") {
+        window.location.href = 'AfterSearch';
+    } else if (val === "") {
+        window.location.href = 'Index';
+    } else {
+        window.location.href = 'ItemNotFound';
+    }
+}
