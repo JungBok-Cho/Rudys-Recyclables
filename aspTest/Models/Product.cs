07ad0e451b64dced12062ef05be7bb16c5cae479
JungBok Cho <chojb2015@gmail.com>
2020-05-28 23:57:36 -0700

    Add comments to the Product.cs file
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/8uuwBtSrZU2KVv0VI08Iv2QAMPgW?Type=TaskLink&Channel=Link&CreatedTime=637263322302520000

--- aspTest/Models/Product.cs
+++ aspTest/Models/Product.cs
@@ -7,18 +7,35 @@ using System.Threading.Tasks;
 
 namespace aspTest.Models
 {
+    /// <summary>
+    /// The Product Page
+    /// </summary>
     public class Product
     {
+        // The product id 
         public string Id { get; set; }
+
+        // The product maker 
         public string Maker { get; set; }
 
+        // The product image 
         [JsonPropertyName("img")]
         public string Image { get; set; }
+
+        // The product url 
         public string Url { get; set; }
+
+        // The product title 
         public string Title { get; set; }
+
+        // The product description 
         public string Description { get; set; }
+
+        // The product ratings
         public int[] Ratings { get; set; }
 
+        // The string representation of the product
+        // It includes every information above
         public override string ToString() => JsonSerializer.Serialize<Product>(this);
 
     }
