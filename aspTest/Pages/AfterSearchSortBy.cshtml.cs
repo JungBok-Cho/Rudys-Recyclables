9853b40a18c766edc7c2bf6e834aaf03c3939017
JungBok Cho <chojb2015@gmail.com>
2020-05-29 17:35:54 -0700

    Fix some type errors in the AfterSearchSortBy.cshtml.cs file
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/517VaKNOIkS1oOcbK6ZCz2QAGDSD?Type=TaskLink&Channel=Link&CreatedTime=637263957516030000

--- aspTest/Pages/AfterSearchSortBy.cshtml.cs
+++ aspTest/Pages/AfterSearchSortBy.cshtml.cs
@@ -8,12 +8,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace RudysRecyclables.Pages
 {
     /// <summary>
-    /// The After Search Page
+    /// The After Search Sort By Page
     /// </summary>
     public class AfterSearchSortByModel : PageModel
     {
         /// <summary>
-        /// Get for the After Search Page
+        /// Get for the After Search Sort By Page
         /// </summary>
         public void OnGet()
         {
