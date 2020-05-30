3344ee94041792b2c80fbfc14b143bb892fa96b1
JungBok Cho <chojb2015@gmail.com>
2020-05-29 18:51:25 -0700

    Write comments on the Startup.cs file
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/517VaKNOIkS1oOcbK6ZCz2QAGDSD?Type=TaskLink&Channel=Link&CreatedTime=637264002804180000

--- aspTest/Startup.cs
+++ aspTest/Startup.cs
@@ -12,23 +12,38 @@ using Microsoft.Extensions.Hosting;
 
 namespace aspTest
 {
+    /// <summary>
+    /// The Startup class
+    /// </summary>
     public class Startup
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="configuration"></param>
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
+        // The configuration of this program
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddTransient<JsonFileProductService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
